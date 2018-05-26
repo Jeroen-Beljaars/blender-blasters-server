2e5d66dfa83aef4a59002409ee218a4dfd6281b7
root <Jeroen>
2018-05-26 19:42:23 +0000

    finished setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ if 'win' in operating_system:
     print("Sorry setup.py is not supported on windows\n" \
           "the server is ment to run on a linux server!")
 elif 'linux' in operating_system:
-    run("apt-get install python-pip")
-    run("pip3 install -r requirements.txt")
+    run("sudo apt-get update && sudo apt-get -y upgrade", shell=True)
+    run("sudo apt-get install python3-pip", shell=True)
+    run("pip3 install -r requirements.txt", shell=True)
 
     # import the installed packages
     import requests
-    from pynput.keyboard import Key, Controller
     
     ip = requests.get("http://ipecho.net/plain?").text
     network_config = {
@@ -26,15 +26,6 @@ elif 'linux' in operating_system:
     with open('network_config.json', 'w') as network_json:
         json.dump(network_config, network_json)
     
-    run("sudo apt-get update")
-    run("sudo apt-get install screen")
-    run("screen")
-    run("python3 manage_servers.py")
-
-    keyboard = Controller()
-    keyboard.press(Key.ctrl)
-    keyboard.press('a')
-    keyboard.release(Key.ctrl)
-    keyboard.release('a')
-    keyboard.press('d')
-    os.system('echo the server is running you can close the terminal now')
+    run("sudo apt-get update", shell=True)
+    run("sudo apt-get install screen", shell=True)
+    run("screen", shell=True)
